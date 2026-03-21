e97a9c3195303eaa49f8c95a8b57f9d66b01801b
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2026-03-21 15:32:07 -0400

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Home from './components/Home.jsx'
-
-import './App.css'
+import NavBar from './components/NavBar.jsx'
+ 
+ 
 
 function App() {
 
     return (
         <div className="App">
+            <NavBar />
             <Home />
         </div>
     );
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import '../App.css';
+
+const NavBar = () => {
+
+  return (
+     <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/products">Products</a></li>
+            <li><a href="/about">Cart</a></li>
+        </ul>
+
+     </nav>
+  );
+  
+};
+
+export default NavBar;
