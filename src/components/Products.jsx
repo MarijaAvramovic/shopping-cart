7b3d9a7af1ab4b4cbcbfbb2b22a8c834faf1de9c
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2026-03-21 18:17:51 -0400

    products add card and style it

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,36 @@
+import "./styles/Products.css";
+
 const Products = () => {
 
   return (
-        <section>
-        <p>Products section</p>
+        <section className="productsSection">
+        <h2>Products section</h2>
+       <Card />
+           
+
         </section>
   );
   
 };
 
 export default Products;
+
+
+
+const Card = () => {
+
+  return (
+        <div className="card">
+            <div className="img"><img src="" alt="" /></div>
+            <h3>Product Title</h3>
+            <p>Product description goes here.</p>
+            <div className="quantity-control">
+                <button>-</button>
+                <input type="number" defaultValue="1" />
+                <button>+</button>
+            </div>
+            <button className="add-to-cart">Add To Cart</button>
+        </div>
+  );
+  
+};
