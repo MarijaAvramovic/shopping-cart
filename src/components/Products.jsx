de4db01b62d1391f8968d7d599894b7e4338b158
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2026-03-21 20:29:37 -0400

    fetch products and add layout for cards on products page

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -25,18 +25,16 @@ const Products = () => {
   return (
         <section className="productsSection">
         <h2>Products</h2>
-        <ul>
-            {products.map(product => (
-                <li key={product.id}>
-                    <h3>{product.category}</h3>
-                    <p>{product.title}</p>
-                    <p>${product.price}</p> 
-
-                </li>
-            ))}
-        </ul>
-       <Card products={products}
-       cartItems={cartItems} setCartItems={setCartItems} />
+         
+                {products.map(product => (
+                  <div key={product.id}>    
+                        <Card  product={product} cartItems={cartItems} setCartItems={setCartItems} />
+                   </div>
+                ))  
+            }        
+         
+        
+     
                
         </section>
   );
@@ -47,7 +45,7 @@ export default Products;
 
 
 
-const Card = ( { products, cartItems, setCartItems } ) => {
+const Card = ( { product, cartItems, setCartItems } ) => {
         const [quantity, setQuantity] = useState(1);
 
          const handleIncrease = () => {
@@ -60,19 +58,21 @@ const Card = ( { products, cartItems, setCartItems } ) => {
 
          const addToCart = () => {
             const newItem = {
-                id: cartItems.length + 1,
-                name: `Product ${cartItems.length + 1}`,
-                price: 19.99,
-                quantity: quantity
+                id: product.id,
+                name: product.title,
+                price: product.price,
+                quantity: quantity,
+                image: product.image,
             };
             setCartItems(prev => [...prev, newItem]);
          }
 
 return (
+
             <div className="card">
-                    <div className="img"><img src={products[4].image} alt="" /></div>
-                    <h3>dsjsdk</h3>
-                    <p>dsds</p>
+                    <div className="img"><img src={product.image} alt={product.title} /></div>
+                    <h3>{product.title}</h3>
+                    <p>{product.category}</p>
                     <div className="quantity-control">
                             <button onClick={handleDecrease}>-</button>
                             <input type="number" value={quantity} onChange={(e) => setQuantity(
